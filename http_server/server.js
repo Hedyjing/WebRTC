57840abdda360bffd8377c6bcc14aedd425ed85b
Hedyjing <374884275@qq.com>
2021-10-25 16:03:10 +0800

    部署在云服务器上

--- http_server/server.js
+++ http_server/server.js
@@ -2,7 +2,8 @@
 
 let http = require('http')
 let app = http.createServer((req, res) => {
+  console.log("http request!!!")
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end("Hello World\n")
-}).listen(8080, "0.0.0.0")
+}).listen(8080)
 console.log("server已启动: 8080")
--- https_server/server.js
+++ https_server/server.js
@@ -4,13 +4,14 @@ let https = require('https')
 let fs = require('fs')
 
 let options = {
-  key: fs.readFileSync('./cert/key.pem'),
-  cert: fs.readFileSync('./cert/cert.pem')
+  key: fs.readFileSync('./cert/hedyu.xyz.key'),
+  cert: fs.readFileSync('./cert/hedyu.xyz.pem')
 }
 
 let app  = https.createServer(options,(req, res) => {
+  console.log("request!!!")
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end("Hello Wrold!\n")
-}).listen(443, '0.0.0.0')
+}).listen(443, "0.0.0.0")
 
 console.log('https服务已启动: 443')
